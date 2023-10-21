fdb2d6871dfff87ecde75479c6f02619ea812b38
Cricket742 <134722302+Cricket742@users.noreply.github.com>
2023-10-20 21:50:23 -0400

    commit player stuff

--- Characters/player.cs
+++ Characters/player.cs
@@ -3,8 +3,11 @@ using System;
 
 public partial class player : CharacterBody2D
 {
-    [Export]
-    public int Speed { get; set; } = 400; // How fast the player will move (pixels/sec).
-
+   [Export] public int Speed { get; set; } = 400; // How fast the player will move (pixels/sec).
     public Vector2 ScreenSize; // Size of the game window.
+
+    public override void _Ready(){
+        ScreenSize = GetViewportRect().Size;
+    }
+    
 }
