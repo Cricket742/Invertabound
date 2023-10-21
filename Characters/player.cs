32cc85b5c7f7574fd30b8120a1e61795d4855254
Cricket742 <134722302+Cricket742@users.noreply.github.com>
2023-10-20 22:12:52 -0400

    player still no worky

--- Characters/player.cs
+++ Characters/player.cs
@@ -9,5 +9,26 @@ public partial class player : CharacterBody2D
     public override void _Ready(){
         ScreenSize = GetViewportRect().Size;
     }
+    public override void _Process(double delta){
+    var velocity = Vector2.Zero; // The player's movement vector.
+        if (Input.IsActionPressed("move_right")){
+            velocity.X += 1;
+        }
+
+        if (Input.IsActionPressed("move_left")){
+            velocity.X -= 1;
+        }
+
+        if (Input.IsActionPressed("move_down")){
+            velocity.Y += 1;
+        }
+
+        if (Input.IsActionPressed("move_up")){
+            velocity.Y -= 1;
+        }
+        if (velocity.Length() > 0){
+            velocity = velocity.Normalized() * Speed;
+        }
+    }
     
 }
